b254fe9e967e4b580cdfe9dd69ae944d5867bf9e
Nimbfire <nimbfire@gmail.com>
2016-04-09 13:52:46 -0300

    Create whatsappweb.js
    load an external jquery
    tests if can prepend something to audio

---
+++ whatsappweb.js
@@ -0,0 +1,11 @@
+// Allow to download the audios from whatsapp web.
+
+console.log('Loading');
+
+// Get jQuery =P
+var jq = document.createElement('script');
+jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+// jQuery('.audio').prepend('<div>lol</div>')
+
