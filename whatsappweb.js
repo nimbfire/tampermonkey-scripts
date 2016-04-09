24d0f84e33e4549c215e953ee534b87b0253bfb4
Nimbfire <nimbfire@gmail.com>
2016-04-09 14:42:22 -0300

    Update whatsappweb.js
    Added a function to get all chat, and added some buttons

--- whatsappweb.js
+++ whatsappweb.js
@@ -9,6 +9,7 @@ document.getElementsByTagName('head')[0].appendChild(jq);
 
 
 setTimeout(addDownloadLinks, 2000);
+addButtonns();
 
 // Add a download link to each audio that has a src. It calls itself again.
 function addDownloadLinks() {
@@ -29,4 +30,22 @@ function addDownloadLinks() {
   
   // Call again in 2 sec
   tid = setTimeout(addDownloadLinks, 2000);
+}
+
+// Add buttons
+function addButtonns(){
+  jQuery('.pane-chat-header').prepend('<div id="download-all">Download all audios</div>');
+  jQuery('.pane-chat-header').prepend('<div id="get-all-chat">Get all chat</div>');
+
+  jQuery('#get-all-chat').on('click', function(){
+    getAllChat();
+  });
+  
+}
+
+
+// Roll the chat until the first ever.
+function getAllChat() {
+  jQuery('.pane-chat-tile-container .pane-chat-msgs').animate({scrollTop:0}, '500');
+  setTimeout(getAllChat, 2000);
 }
