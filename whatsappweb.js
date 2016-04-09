973a01d42a8f3f7abe000b624ec9801077a1f139
Nimbfire <nimbfire@gmail.com>
2016-04-09 14:12:07 -0300

    Update whatsappweb.js
    Add the download link to audios with the src. Calls itself after 2 seconds.

--- whatsappweb.js
+++ whatsappweb.js
@@ -7,5 +7,26 @@ var jq = document.createElement('script');
 jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
 document.getElementsByTagName('head')[0].appendChild(jq);
 
-// jQuery('.audio').prepend('<div>lol</div>')
 
+setTimeout(addDownloadLinks, 2000);
+
+// Add a download link to each audio that has a src. It calls itself again.
+function addDownloadLinks() {
+  jQuery('.audio').each(function(){
+    var currentElement = $(this);
+    var src = currentElement.find('audio').attr('src');
+    if (src) {
+      if (currentElement.hasClass('download')) {
+        
+      }
+      else {
+        var el = '<a href="' + src + '">Download</a>';
+        currentElement.prepend(el);
+        currentElement.addClass('download');
+      }
+    }
+  });
+  
+  // Call again in 2 sec
+  tid = setTimeout(addDownloadLinks, 2000);
+}
